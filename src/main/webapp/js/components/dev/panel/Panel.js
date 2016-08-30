e3a06684cb4a5e79ed2389ecefb2cefb1d6d03d5
Adrian Quintana <adrianquintana@gmail.com>
2016-08-30 08:45:25 +0100

    autoresize for parent panel

--- src/main/webapp/js/components/dev/panel/Panel.js
+++ src/main/webapp/js/components/dev/panel/Panel.js
@@ -85,6 +85,14 @@ define(function (require) {
         	currentStyle['flexDirection'] = direction;
         	this.setState({ parentStyle:  currentStyle});
         },
+        
+        componentDidMount : function(){
+        	var comp = $('#' + this.props.id);
+        	if (comp.parent().hasClass('dialog')){
+        		comp.parent().height(comp.height() + 10);
+        		comp.parent().parent().width(comp.width() + 70);
+        	}
+        },
 		
          render: function(){
         	 var itemComponents = this.state.items.map(function (item) {		            			 
